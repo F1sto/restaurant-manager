e8f063e2a6e663b4c6e03af2f8162847660df804
andrei misyurin <andrew.misiyrin@gmail.com>
2024-04-25 23:08:13 +0600

    feat: Request to server

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [serverMessage, setMessage] = useState('')
+
+  async function fetchHelloData() {
+    const res = await fetch('http://localhost:3000/get-hello');
+    const {message} = await res.json();
+    setMessage(message);
+  }
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo"/>
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -25,8 +32,25 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
+      <div className='server-message-card'>
+        <span className='card-title'>The smartest response is: </span>
+
+        <span>{serverMessage}</span>
+      </div>
+
+      <div className='buttons-wrapper'>
+        <button className='ugly-btn' onClick={fetchHelloData}>
+          get response
+        </button>
+
+        <button className='ugly-btn gray' onClick={() => setMessage('')}>
+          clear
+        </button>
+      </div>
+
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click to most beautiful button to get server response
       </p>
     </>
   )
